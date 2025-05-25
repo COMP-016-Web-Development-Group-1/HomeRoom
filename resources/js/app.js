b9b92858e3958dd8db4d8cb1e00425c157bbe7bd
John Paul <johnpaulviado20@gmail.com>
2025-05-25 12:56:07 +0800

    feat: enable asset globbing for improved asset management

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import "@phosphor-icons/web/bold";
 import "@phosphor-icons/web/fill";
 import "@phosphor-icons/web/regular";
 
-// import.meta.glob("../assets/**/*");
+import.meta.glob("../assets/**/*");
 // import.meta.glob(["../assets/fonts/**/*", "../assets/images/**/*"]);
 
 window.Alpine = Alpine;
