c879da97a25516a133a2aef2a45854a17a3ceda5
John Paul <johnpaulviado20@gmail.com>
2025-05-29 01:15:14 +0800

    feat: refactor registration test as it is now a multi-step form

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -16,16 +16,36 @@ test('registration screen can be rendered', function () {
 test('new tenants can register', function () {
     $room = Room::factory()->create();
 
-    $response = $this->post('/register', [
+    $step1Response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@gmail.com',
         'profile_picture' => UploadedFile::fake()->image('my-profile.jpg'),
+    ]);
+
+    $step1Response->assertRedirect('/register/2');
+
+    // Step 2: Password
+    $step2Response = $this->post('/register/2', [
         'password' => 'password',
         'password_confirmation' => 'password',
+    ]);
+
+    $step2Response->assertRedirect('/register/3');
+
+    // Step 3: Room Code
+    $step3Response = $this->post('/register/3', [
         'code' => $room->code,
     ]);
 
+    $step3Response->assertRedirect(route('dashboard', absolute: false));
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
     Storage::disk('public')->assertExists(auth()->user()->profile_picture);
+});
+
+test('invalid registration fails', function () {
+    $response = $this->get('/register/0');
+    $response->assertNotFound();
+
+    $response2 = $this->get('/register/abc');
+    $response2->assertNotFound();
 });
