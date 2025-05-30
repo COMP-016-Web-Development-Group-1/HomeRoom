9f8221d7b5ebe2193d5026b5f7c9728f287bd81e
John Paul <johnpaulviado20@gmail.com>
2025-05-30 17:00:09 +0800

    fix: update position of the foregin key user

--- database/migrations/2025_05_20_093002_create_landlords_table.php
+++ database/migrations/2025_05_20_093002_create_landlords_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('landlords', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->string('gcash_qr')->nullable();
             $table->string('maya_qr')->nullable();
-            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
