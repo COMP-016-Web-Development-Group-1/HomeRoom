eea1231613a7421445bbaf431f3c8ee23b188ddf
John Paul <johnpaulviado20@gmail.com>
2025-05-30 20:10:23 +0800

    feat: add CheckProfileCompletion middleware to enforce profile completion for landlords

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'profile-completed' => \App\Http\Middleware\CheckProfileCompletion::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
