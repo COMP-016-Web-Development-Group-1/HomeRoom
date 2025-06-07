5da262ec5e544dcba790fbdff40002f0274001bb
John Paul <johnpaulviado20@gmail.com>
2025-06-08 02:19:11 +0800

    feat: add flowbite plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
+import flowbitePlugin from "flowbite/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,8 +8,10 @@ export default {
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
+        "./node_modules/flowbite/**/*.js",
     ],
 
+    darkMode: "selector",
     theme: {
         extend: {
             fontFamily: {
@@ -25,8 +28,24 @@ export default {
             animation: {
                 shrink: "shrink 3.2s linear forwards",
             },
+
+            // colors: {
+            //     primary: {
+            //         50: "#f7fee7",
+            //         100: "#ecfccb",
+            //         200: "#d9f99d",
+            //         300: "#bef264",
+            //         400: "#a3e635",
+            //         500: "#84cc16",
+            //         600: "#65a30d",
+            //         700: "#4d7c0f",
+            //         800: "#3f6212",
+            //         900: "#365314",
+            //         950: "#1a2e05",
+            //     },
+            // },
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, flowbitePlugin],
 };
