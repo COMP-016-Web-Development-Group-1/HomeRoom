e9062cef2f4113ee0f3867f385d93b48c074291a
John Paul <johnpaulviado20@gmail.com>
2025-06-10 14:11:23 +0800

    refactor: remove unused file input and date picker from properties index

--- resources/views/landlord/property/index.blade.php
+++ resources/views/landlord/property/index.blade.php
@@ -6,12 +6,6 @@
     </x-slot>
 
     <div class="max-w-(--breakpoint-2xl) mx-auto sm:px-6 lg:px-8">
-        <input type="file" />
-        <div class="mt-4 mb-4">
-            <x-input.date maxDate="{{ iso_to_us(now()->toDateString()) }}" :withButtons="true" name="birth_date"
-                :value="old('birth_date')" placeholder="Select Date" />
-        </div>
-
         <div class="bg-white shadow-xs sm:rounded-lg">
             <div class="p-6 text-gray-900">
                 Properties (Index)
