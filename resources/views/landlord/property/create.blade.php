d1b73f1e5b0171ea453d2ce076d9c5d977659963
John Paul <johnpaulviado20@gmail.com>
2025-06-11 20:52:17 +0800

    fix: updated form input value retrieval to use old() for better data handling and changed layout component in edit view

--- resources/views/landlord/property/create.blade.php
+++ resources/views/landlord/property/create.blade.php
@@ -15,7 +15,7 @@
 
             <div class="mb-2 p-2">
                 <x-input.label for="name" :required="true">Property Name</x-input.label>
-                <x-input.text id="name" type="text" name="name" :value="old('name', $data['name'] ?? '')" autofocus />
+                <x-input.text id="name" type="text" name="name" :value="old('name')" autofocus />
                 <x-input.error for="name" />
             </div>
 
@@ -28,21 +28,21 @@
                         'dorm' => 'Dorm',
                         'condominium' => 'Condominium', ]"
                     :selected="old('type')"
-                    :value="old('name', $data['type'] ?? '')" />
+                    :value="old('type')" />
                 <x-input.error for="type" />
             </div>
 
             <div class="mb-2 p-2">
                 <x-input.label for="address" :required="true">Property Address</x-input.label>
-                <x-input.text id="address" type="text" name="address" :value="old('name', $data['address'] ?? '')" />
+                <x-input.text id="address" type="text" name="address" :value="old('address')" />
                 <p class="text-xs text-center">Building/Unit/House Number, Street Name, Barangay, City/Municipality, Province, ZIP Code</p>
                 <x-input.error for="address" />
             </div>
 
             <div class="mb-2 p-2">
                 <x-input.label for="description" :required="true">Description</x-input.label>
-                <x-input.textarea id="description" name="description" rows="4" :value="old('name', $data['description'] ?? '')" />
-                <x-input.error for="address" />
+                <x-input.textarea id="description" name="description" rows="4" :value="old('description')" />
+                <x-input.error for="description" />
             </div>
 
             <div class="flex items-end justify-between gap-4 mb-4 mt-4 p-4">
@@ -52,7 +52,7 @@
         </form>
     </div>
 
-</x-guest-layout>
+</x-app-layout>
 
 <script>
     document.getElementById('cancelBtn').addEventListener('click', function (e) {
