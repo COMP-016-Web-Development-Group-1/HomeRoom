3207215ffda3a79e9c5fdf652e4dec2c0cd9c82c
RAbnza <RAbnza@users.noreply.github.com>
2025-06-14 07:55:15 +0000

    PHP Linting (Pint)

--- routes/web.php
+++ routes/web.php
@@ -85,4 +85,4 @@ Route::middleware(['auth', 'verified', 'profile-completed'])->group(function () 
     Route::view('/tests', 'table');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
