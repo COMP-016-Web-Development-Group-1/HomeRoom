36a7b03c3ce54b082a55ea02aa5e1f1978db285b
John Paul <johnpaulviado20@gmail.com>
2025-06-14 18:36:16 +0800

    fix: change due_date column type from date to timestamp in bills migration

--- database/migrations/2025_06_12_013214_create_bills_table.php
+++ database/migrations/2025_06_12_013214_create_bills_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,12 +16,13 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Tenant::class)->constrained()->onDelete('cascade');
             $table->decimal('amount_due', 10, 2);
-            $table->date('due_date');
+            $table->timestamp('due_date');
             $table->enum('status', array_column(BillStatus::cases(), 'value'))
                 ->default(BillStatus::UNPAID->value);
             $table->timestamps();
-        });
 
+            $table->unique(['tenant_id', 'created_at'], 'unique_tenant_monthly_bill');
+        });
     }
 
     /**
